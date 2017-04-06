5b3aaed7899401039e1e79c981b38df5401aeca3
Dominic Philip <domi.a.philip@gmail.com>
2017-04-06 18:01:41 -0400

    format code with pre-commit

--- src/arbitrage/arbitrage.go
+++ src/arbitrage/arbitrage.go
@@ -1,5 +1,19 @@
 package arbitrage
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+func GetFullCurrencyName(identifier string) string {
+	response, err := http.Get("https://openexchangerates.org/api/currencies.json")
+	if err != nil {
+		panic(err)
+	}
+    defer response.Body.Close()
+
+}
+
+func main() {
+
+}
