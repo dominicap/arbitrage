72e8c99999ff9214e135284895eb494fc3f96d2b
Dominic Philip <domi.a.philip@gmail.com>
2017-04-08 11:22:34 -0400

    half complete flag parser

--- arbitrage.go
+++ arbitrage.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
+	"os"
+	"path"
+	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -17,22 +23,30 @@ const (
 
 var openExchangeRatesKey = keys()[0]
 
-func currencyCode(name string) string {
+var (
+	code  string
+	name  string
+	value float64
+)
+
+var err error
+
+func currencyCode(name string) (string, error) {
 	name = strings.Title(name)
 	for key, value := range currencyMap() {
 		if name == value {
-			return key
+			return key, nil
 		}
 	}
-	panic("Currency name not found.")
+	return "", errors.New("currency name not found")
 }
 
-func currencyName(code string) string {
+func currencyName(code string) (string, error) {
 	code = strings.ToUpper(code)
 	if currencyMap()[code] != "" {
-		return currencyMap()[code]
+		return currencyMap()[code], nil
 	}
-	panic("Currency code not found")
+	return "", errors.New("currency name not found")
 }
 
 func currencyMap() map[string]string {
@@ -54,11 +68,59 @@ func currencyMap() map[string]string {
 }
 
 func keys() []string {
-	contents, err := ioutil.ReadFile(".keys/keys")
+	_, fileName, _, _ := runtime.Caller(1)
+	filePath := path.Join(path.Dir(fileName), "/.keys/keys")
+
+	contents, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		panic(err.Error())
 	}
+
 	return strings.Split(string(contents), "\n")
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "OVERVIEW: An exploitation of the discrepancies in the foreign exchange markets\n\n")
+	fmt.Fprintf(os.Stderr, "USAGE: arbitrage [value] [currency code]\n\n")
+	fmt.Fprintf(os.Stderr, "OPTIONS:\n")
+	flag.PrintDefaults()
+}
+
+var (
+	helpFlag = flag.Bool("help", false, "Displays the help message.")
+	versionFlag = flag.Bool("version", false, "Displays the version number.")
+)
+
+func init() {
+	flag.Usage = usage()
+	flag.Parse()
+
+	if flag.NArg() == 1 {
+		panic("must provide arguments")
+	}
+	value, err = strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		panic("monetary value must be a number")
+	}
+	if value == 0 {
+		panic("value must be greater than 0")
+	}
+	value = toFixed(value, 2)
+
+	code = os.Args[2]
+	name, err = currencyName(code)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func main() {
